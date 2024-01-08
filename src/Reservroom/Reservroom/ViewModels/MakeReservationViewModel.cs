29237847035dcba547a510045e0f3a67b280bf31
Bruno Pouliot <dev.lecanardnoir@gmail.com>
2024-01-07 22:24:17 -0500

    FCT: Basic Error feedback

--- src/Reservroom/Reservroom/ViewModels/MakeReservationViewModel.cs
+++ src/Reservroom/Reservroom/ViewModels/MakeReservationViewModel.cs
@@ -3,7 +3,9 @@ using Reservroom.Models;
 using Reservroom.Services;
 using Reservroom.Stores;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,7 @@ using System.Windows.Input;
 
 namespace Reservroom.ViewModels
 {
-    public class MakeReservationViewModel : ViewModelBase
+    public class MakeReservationViewModel : ViewModelBase, INotifyDataErrorInfo
     {
         private string _username;
         public string Username 
@@ -47,17 +49,26 @@ namespace Reservroom.ViewModels
         }
 
         private DateTime _startDate = DateTime.Now;
-        public DateTime StatDate
+        public DateTime StartDate
         {
             get { return _startDate; }
             set
             {
                 _startDate = value;
-                OnPropertyChanged(nameof(StatDate));
+                OnPropertyChanged(nameof(StartDate));
+
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (StartDate > EndDate)
+                {
+                    AddErrors("The start date cannot be after the end date.", nameof(StartDate));
+                }
             }
         }
 
         private DateTime _endDate = DateTime.Now.AddDays(1);
+
         public DateTime EndDate
         {
             get { return _endDate; }
@@ -65,17 +76,59 @@ namespace Reservroom.ViewModels
             {
                 _endDate = value;
                 OnPropertyChanged(nameof(EndDate));
+
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (EndDate < StartDate)
+                {
+                    //AddErrors("The start date cannot be after the end date.", nameof(StartDate));
+                    AddErrors("The end date cannot be before the start date.", nameof(EndDate));
+                }
+
             }
         }
 
         public ICommand SubmitCommand { get; }
         public ICommand CancelCommand { get; }
 
+        private readonly Dictionary<string, List<string>> _propertyNameToErrorsDictionnary;
+        public bool HasErrors => _propertyNameToErrorsDictionnary.Any();
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
         public MakeReservationViewModel(HotelStore hotelStore, NavigationServices reservationViewNavigationServices)
         {
             SubmitCommand = new MakeReservationCommand(this, hotelStore, reservationViewNavigationServices);
             CancelCommand = new NavigateCommand(reservationViewNavigationServices);
+            _propertyNameToErrorsDictionnary = new Dictionary<string, List<string>>();
         }
 
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _propertyNameToErrorsDictionnary.GetValueOrDefault(propertyName, new List<string>());
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            _propertyNameToErrorsDictionnary.Remove(propertyName);
+            OnErrorsChanged(propertyName);
+        }
+
+        private void AddErrors(string errorMessage, string propertyName)
+        {
+            if(!_propertyNameToErrorsDictionnary.ContainsKey(propertyName))
+            {
+                _propertyNameToErrorsDictionnary.Add(propertyName, new List<string>());
+            }
+
+            _propertyNameToErrorsDictionnary[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
     }
 }
